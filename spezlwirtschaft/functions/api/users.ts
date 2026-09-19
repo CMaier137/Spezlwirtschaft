83e0b1dffc91bdf1dc875265f15bba5a35b0735c
CMaier137 <christian.maier.88@gmail.com>
2026-09-19 22:51:01 +0200

    Update users.ts

--- spezlwirtschaft/functions/api/users.ts
+++ spezlwirtschaft/functions/api/users.ts
@@ -1,6 +1,6 @@
 import { Env, json } from './_shared'
 
 export const onRequestGet: PagesFunction<Env> = async ({ env }) => {
-  const { results } = await env.DB.prepare('SELECT id, name FROM users ORDER BY name').all()
+  const { results } = await env.DB.prepare('SELECT id, name FROM users ORDER BY sort_order').all()
   return json(results)
 }
